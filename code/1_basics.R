729db42e7abc83d39106e3313ad656bdf10fb57f
Kyle M. Lang <k.m.lang@uu.nl>
2023-01-25 11:44:52 +0100

    converting labs into stand-alone interactive scripts; finished with the first two

---
+++ code/1_basics.R
@@ -0,0 +1,211 @@
+### Title:    Regression in R: Basics
+### Author:   Kyle M. Lang
+### Created:  2017-09-08
+### Modified: 2023-01-25
+
+
+###-Preliminaries------------------------------------------------------------###
+
+rm(list = ls(all = TRUE)) # Clear workspace
+
+library(MLmetrics) # For MSEs
+library(dplyr)     # For pipelines
+
+
+###-Data I/O-----------------------------------------------------------------###
+
+dataDir <- "data/"
+cars    <- readRDS(paste0(dataDir, "mtcars.rds"))
+
+################################################################################
+## PRACTICE PROBLEM 1.1
+##
+## Use the readRDS() function to load the "longley.rds" data.
+##
+## NOTE: Unless otherwise specified, these data are used for all subsequent
+##       practice problems.
+##
+################################################################################
+
+
+###-Simple Linear Regression-------------------------------------------------###
+
+## Fit a simple linear regression model:
+out1 <- lm(qsec ~ hp, data = cars)
+
+## See what's inside the fitted lm object:
+ls(out1)
+
+## Summarize the results:
+summary(out1)
+
+## The 'summary' output is an object that we can save to a variable:
+s1 <- summary(out1)
+ls(s1)
+s1
+
+## Access the R^2 slot in the summary object:
+s1$r.squared
+
+## Extract coefficients:
+coef(out1)
+
+## Extract residuals:
+res3 <- resid(out1)
+res3
+
+## Extract fitted values (two ways):
+yHat1.1 <- fitted(out1)
+yHat1.2 <- predict(out1)
+
+yHat1.1
+yHat1.2
+
+## Compare:
+yHat1.1 - yHat1.2
+
+################################################################################
+## PRACTICE PROBLEM 1.2
+##
+## a) Regress "GNP" onto "Year".
+## b) What is the slope of "Year" on "GNP"?
+## c) Is the effect of "Year" on "GNP" statistically significant at the
+##    alpha = 0.05 level?
+## d) Is the effect of "Year" on "GNP" statistically significant at the
+##    alpha = 0.01 level?
+##
+################################################################################
+
+
+###-Multiple Linear Regression-----------------------------------------------###
+
+## Fit a multiple linear regression model:
+out4 <- lm(qsec ~ hp + wt + carb, data = cars)
+
+## Summarize the model:
+s4 <- summary(out4)
+s4
+
+## Extract R^2:
+s4$r.squared
+
+## Extract F-stat:
+s4$fstatistic
+
+## Extract coefficients:
+coef(out4)
+
+## Compute confidence intervals for coefficients:
+confint(out4)
+confint(out4, parm = "hp")
+confint(out4, parm = c("(Intercept)", "wt"), level = 0.99)
+
+## Manually compute t-statistics:
+cf4 <- coef(out4)
+se4 <- out4 %>% vcov() %>% diag() %>% sqrt()
+t4  <- cf4 / se4
+
+s4
+t4
+
+################################################################################
+## PRACTICE PROBLEM 1.3
+##
+## a) Regress "GNP" onto "Year" and "Population".
+## b) Is there a significant effect (at the alpha = 0.05 level) of "Year" on
+##    "GNP", after controlling for "Population"?
+## c) Is there a significant effect (at the alpha = 0.01 level) of "Year" on
+##    "GNP", after controlling for "Population"?
+## d) What is the 95% confidence interval (CI) for the partial effect of
+##    "Population" on "GNP"?
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 1.4
+##
+## a) Regress "GNP" onto "Year" and "Employed".
+## b) What is the partial effect of "Employed" on "GNP", after controlling for
+##    "Year"?
+## c) Is the partial effect of "Employed" on "GNP" statistically significant at
+##    the alpha = 0.05 level?
+## d) What is the 99% CI for the partial effect of "Employed" on "GNP"?
+##
+################################################################################
+
+
+###-Model Comparison---------------------------------------------------------###
+
+## Change in R^2:
+s4$r.squared - s1$r.squared
+
+## Significant increase in R^2?
+anova(out1, out4)
+
+## Compare MSE values:
+mse1 <- MSE(y_pred = predict(out1), y_true = cars$qsec)
+mse4 <- MSE(y_pred = predict(out4), y_true = cars$qsec)
+
+mse1
+mse4
+
+################################################################################
+## PRACTICE PROBLEM 1.5
+##
+## a) What is the difference in R-squared between the simple linear regression
+##    of "GNP" onto "Year" and the multiple linear regression of "GNP" onto
+##    "Year" and "Population"?
+## b) Is this increase in R-squared significantly different from zero at the
+##    alpha = 0.05 level?
+## c) What is the value of the test statistic that you used to answer (1b)?
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 1.6
+##
+## a) What is the MSE for the model that regresses "GNP" onto "Year" and
+##    "Population"?
+## b) What is the MSE for the model that regresses "GNP" onto "Year" and
+##    "Employed"?
+## c) According to the MSE values calculated above, is "Population" or
+##    "Employed" the better predictor of "GNP"?
+## d) Could we do the comparison in (c) using an F-test for the difference in
+##    R-squared values? Why or why not?
+##
+################################################################################
+
+
+###-A More Elegant Way to Build Models---------------------------------------###
+
+## The long way:
+out1 <- lm(qsec ~ hp, data = mtcars)
+out2 <- lm(qsec ~ hp + wt, data = mtcars)
+out3 <- lm(qsec ~ hp + wt + gear + carb, data = mtcars)
+
+## The short way:
+out2.1 <- update(out1, ". ~ . + wt")
+out3.1 <- update(out2.1, ". ~ . + gear + carb")
+out3.2 <- update(out1, ". ~ . + wt + gear + carb")
+
+## We can also remove variables:
+out1.1 <- update(out2, ". ~ . - wt")
+
+summary(out2)
+summary(out2.1)
+
+all.equal(out1, out1.1)
+all.equal(out2, out2.1)
+all.equal(out3, out3.1)
+all.equal(out3, out3.2)
+
+## We can rerun the same model on new data:
+mtcars2 <- mtcars[1 : 15, ]
+
+out4 <- update(out3, data = mtcars2)
+
+summary(out3)
+summary(out4)
+
+
+###-END----------------------------------------------------------------------###
---
+++ code/2_categorical_predictors.R
@@ -0,0 +1,352 @@
+### Title:    Regression in R 2: Categorical Predictors
+### Author:   Kyle M. Lang
+### Created:  2017-09-08
+### Modified: 2023-01-25
+
+rm(list = ls(all = TRUE))
+
+
+###-Preliminaries------------------------------------------------------------###
+
+library(wec) # For weighted effects codes
+
+## Load the iris data
+data(iris)
+
+set.seed(235711)
+
+## Sample 100 rows to unbalance group sizes:
+iris <- iris[sample(1:nrow(iris), 100), ]
+
+################################################################################
+## PRACTICE PROBLEM 2.1
+##
+## Use the data() function to load the "bfi" and "BMI" datasets from the
+## psychTools package.
+##
+################################################################################
+
+
+###-Factor Variables---------------------------------------------------------###
+
+## Look at the 'Species' factor:
+iris$Species
+is.factor(iris$Species)
+
+str(iris$Species)
+summary(iris$Species)
+
+## Factors have special attributes:
+attributes(iris$Species)
+attributes(iris$Petal.Length)
+attributes(iris)
+
+## Factors have labeled levels:
+levels(iris$Species)
+nlevels(iris$Species)
+
+## Factors are not numeric variables:
+mean(iris$Species)
+var(iris$Species)
+iris$Species - iris$Species
+
+################################################################################
+## PRACTICE PROBLEM 2.2
+##
+## Use the "bfi" data to complete the following tasks.
+##
+## a) Refer to the help file of the "bfi" dataset to find the correct levels for
+##    the "gender" and "education" variables.
+## b) Create factors for the "gender" and "education" variables with sensible
+##    sets of labels for the levels.
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.3
+##
+## Use the "bfi" data to complete the following tasks.
+##
+## a) How many women in this sample have graduate degrees?
+## b) What is the most frequently reported level of educational attainment among
+##    men in this sample?
+##
+################################################################################
+
+
+###-Dummy Codes--------------------------------------------------------------###
+
+## Use a factor variable as a predictor:
+out1 <- lm(Petal.Length ~ Species, data = iris)
+summary(out1)
+
+## Check the contrasts:
+contrasts(iris$Species)
+
+## Change the reference group:
+iris$Species2 <- relevel(iris$Species, ref = "virginica")
+
+levels(iris$Species)
+levels(iris$Species2)
+
+## How are the contrasts affected?
+contrasts(iris$Species)
+contrasts(iris$Species2)
+
+## Which carries through to the models:
+out2 <- lm(Petal.Length ~ Species2, data = iris)
+summary(out1)
+summary(out2)
+
+################################################################################
+## PRACTICE PROBLEM 2.4
+##
+## Use the "BMI" data to complete the following tasks.
+##
+## a) How many levels does the "education" factor have?
+## b) What is the reference level of the "sex" factor?
+## c) What is the reference level of the "education" factor?
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.5
+##
+## Use the "BMI" data to complete the following tasks.
+##
+## a) Run a linear regression model wherein "BMI" is predicted by dummy-coded
+##    versions of "sex" and "education".
+##    -- Set the reference group to "male" for "sex".
+##    -- Set the reference group to "highest" for "education".
+## b) Is there a significant effect (at alpha = 0.05) of "sex" on "BMI" after
+##    controlling for "education"?
+## c) According to the model from (3a), what is the expected BMI for males in
+##     the highest education group?
+##
+################################################################################
+
+
+###-Unweighted Effects Codes-------------------------------------------------###
+
+## Use the contr.sum() function to create contrasts for unweighted effects codes
+?contr.sum
+
+iris$Species3            <- iris$Species
+contrasts(iris$Species3) <- contr.sum(levels(iris$Species3))
+contrasts(iris$Species3)
+
+## Use the fancy-pants Species factor:
+out3 <- lm(Petal.Length ~ Species3, data = iris)
+summary(out3)
+contrasts(iris$Species3)
+
+## How about some better names?
+colnames(contrasts(iris$Species3)) <- c("setosa", "versicolor")
+contrasts(iris$Species3)
+
+## OR ##
+
+## Define a function to automatically fix EC names:
+fixEcNames <- function(x) {
+    tmp                    <- contrasts(x)
+    colnames(contrasts(x)) <- rownames(tmp)[rowSums(tmp) > 0]
+    x
+}
+
+iris$Species3 <- fixEcNames(iris$Species3)
+contrasts(iris$Species3)
+
+out4 <- lm(Petal.Length ~ Species3, data = iris)
+summary(out4)
+
+## Change the omitted group:
+iris$Species4 <- iris$Species
+levels(iris$Species4)
+iris$Species4 <- relevel(iris$Species4, ref = "virginica")
+
+levels(iris$Species)
+levels(iris$Species4)
+
+## This won't work:
+tmp <- relevel(iris$Species, ref = "versicolor")
+
+levels(iris$Species)
+levels(tmp)
+
+## Define a function to automatically change the omitted group:
+changeOmitted <- function(x) relevel(x, ref = levels(x)[nlevels(x)])
+
+tmp <- changeOmitted(iris$Species)
+
+levels(iris$Species)
+levels(tmp)
+
+## Update the contrasts attribute:
+contrasts(iris$Species4)
+contrasts(iris$Species4) <- contr.sum(nlevels(iris$Species4))
+contrasts(iris$Species4)
+
+## Give some good names:
+iris$Species4 <- fixEcNames(iris$Species4)
+contrasts(iris$Species4)
+
+## Use the new factor:
+out5 <- lm(Petal.Length ~ Species4, data = iris)
+summary(out4)
+summary(out5)
+
+## To summarize:
+iris$Species4            <- iris$Species
+iris$Species4            <- changeOmitted(iris$Species4)
+contrasts(iris$Species4) <- contr.sum(nlevels(iris$Species4))
+iris$Species4            <- fixEcNames(iris$Species4)
+
+contrasts(iris$Species)
+contrasts(iris$Species3)
+contrasts(iris$Species4)
+
+################################################################################
+## PRACTICE PROBLEM 2.6
+##
+## Use the "BMI" data to estimate the following model.
+##
+## Regress "BMI" onto an unweighted effects-coded representation of "education"
+## and a dummy-coded representation of "childless".
+## - Adjust the contrasts attribute of the "education" factor to implement the
+##   unweighted effects coding.
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.7
+##
+## Change the reference group (i.e., the omitted group) for the unweighted
+## effects codes that you implemented in PP 2.6, and rerun the model regressing
+## "BMI" onto "education" and "childless".
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.8
+##
+## Use the results from the models you estimated in PP 2.6 and PP 2.7 to answer
+## the following questions.
+##
+## a) What is the expected BMI (averaged across education groups) for people
+##    with children?
+## b) What is the expected difference in BMI between the most highly educated
+##    group and the average BMI across education groups, after controlling for
+##    childlessness?
+## c) Is the difference you reported in (b) significantly different from zero,
+##    at the alpha = 0.05 level?
+## d) What is the expected difference in BMI between the middle education group
+##    and the average BMI across education groups, after controlling for
+##    childlessness?
+## e) Is the difference you reported in (d) significantly different from zero,
+##    at the alpha = 0.05 level?
+##
+################################################################################
+
+
+###-Weighted Effects Codes---------------------------------------------------###
+
+## Use the wec::contr.wec() function to create weighted effects-coded contrasts:
+iris$Species5            <- iris$Species
+contrasts(iris$Species5) <- contr.wec(iris$Species, omitted = "virginica")
+contrasts(iris$Species5)
+
+out6 <- lm(Petal.Length ~ Species5, data = iris)
+summary(out6)
+
+## Create contrast with a different reference level:
+iris$Species6            <- iris$Species
+contrasts(iris$Species6) <- contr.wec(iris$Species, omitted = "setosa")
+contrasts(iris$Species6)
+
+out7 <- lm(Petal.Length ~ Species6, data = iris)
+summary(out7)
+summary(out6)
+
+################################################################################
+## PRACTICE PROBLEM 2.9
+##
+## Use the "BMI" data to estimate the following model.
+##
+## Regress "BMI" onto a weighted effects-coded representation of "education" and
+## a dummy-coded representation of "sex".
+## - Adjust the contrasts attribute of the "education" factor to implement the
+##   weighted effects coding.
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.10
+##
+## Change the reference group (i.e., the omitted group) for the weighted effects
+## codes that you implemented in PP 2.9, and rerun the model regressing "BMI"
+## onto "education" and "sex".
+##
+################################################################################
+
+################################################################################
+## PRACTICE PROBLEM 2.11
+##
+## Use the results from the models you estimated in PP 2.9 and PP 2.10 to answer
+## the following questions.
+
+## a) What is the average BMI for females?
+## b) What is the expected difference in BMI between the least educated group
+##    and the average BMI, after controlling for sex?
+## c) Is the difference you reported in (b) significantly different from zero,
+##    at the alpha = 0.01 level?
+## d) What is the expected difference in BMI between the most highly educated
+##    group and the average BMI, after controlling for sex?
+## e) Is the difference you reported in (d) significantly different from zero,
+##    at the alpha = 0.01 level?
+##
+################################################################################
+
+
+###-Reverting to Default "Treatment" Contrasts-------------------------------###
+
+contrasts(iris$Species6)
+
+## Use the contr.treatment() function to get back to dummy codes (default):
+contrasts(iris$Species6) <- contr.treatment(levels(iris$Species6))
+
+contrasts(iris$Species6)
+contrasts(iris$Species)
+
+
+###-Testing the Effects of Grouping Factors----------------------------------###
+
+summary(out1)
+
+## Test the effect of Species:
+out0 <- lm(Petal.Length ~ 1, data = iris)
+
+summary(out0)
+anova(out0, out1)
+
+## Test the partial effect of Species:
+out8.1 <- lm(Petal.Length ~ Petal.Width, data = iris)
+out8.2 <- update(out8.1, ". ~ . + Species")
+
+summary(out8.1)
+summary(out8.2)
+
+anova(out8.1, out8.2)
+
+################################################################################
+## PRACTICE PROBLEM 2.12
+##
+## Use the "BMI" data to answer the following questions.
+##
+## a) Does education level explain a significant proportion of variance in BMI,
+##    above and beyond sex?
+## b) What is the value of the test statistic that you used to answer (a)?
+##
+################################################################################
+
+
+###-END----------------------------------------------------------------------###
